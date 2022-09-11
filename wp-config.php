51eddf549b0623618ce84da2a509484b05e11b78
nnedeljkovic8 <111445814+nnedeljkovic8@users.noreply.github.com>
2022-09-11 18:34:19 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'nnedeljkovic_digitel' );
+define( 'DB_NAME', '' );
 
 /** Database username */
-define( 'DB_USER', 'nnedeljkovic_vezbaDigitel' );
+define( 'DB_USER', '' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'ien{kB(e5k)-' );
+define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
